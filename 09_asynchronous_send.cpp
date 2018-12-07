07fbc5a354b94f81764b36ebfba59e7d261abb37
pengjintao <1272813056@qq.com>
2018-12-07 22:49:52 +0800

    async

--- 09_asynchronous_send.cpp
+++ 09_asynchronous_send.cpp
@@ -8,19 +8,25 @@ int main(int argc,char **argv)
 {
     int rank,size;
     int data_size = 1<<20;
-    char data[data_size];
+  	int node_cpu_num = 20;
+    char *data;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
-    MPI_Request req;
+    MPI_Request req[2];
     MPI_Status status;
+
 
     if(rank == 0)
     {
-        MPI_Send(&data,data_size,MPI_CHAR,size-1,0,MPI_COMM_WORLD);
+    	data = new char [data_size];
+        MPI_Isend(data,data_size,MPI_CHAR,size-1,0,MPI_COMM_WORLD,&req[0]);
         cout<<"send finish"<<endl;
     }else if(rank == size-1){
+    	data = new char [data_size];
         sleep(1);
+        MPI_Irecv(data,data_size,MPI_CHAR,0,0,MPI_COMM_WORLD,&req[1]);
+        MPI_Wait(&req[1],&status);
         //MPI_Recv(&data,128,MPI_CHAR,0,0,MPI_COMM_WORLD,&status);
         cout<<"recv finish"<<endl;
     }
---
+++ 12_MPI_stringlen.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <strinig.h>
+#include <mpi.h>
+
+const int MAX_STRING = 100
+
+int main()
+{
+	char greeting[MAX_STRING];
+	int co
+	return 0;
+}
